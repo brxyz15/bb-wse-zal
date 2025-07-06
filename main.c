1d106b4b3bda60663141b7b283d798decbd3485a
brxyz15 <bbrzoza0@gmail.com>
2025-07-06 12:18:51 +0200

    Update

--- main.c
+++ main.c
@@ -1,103 +1,175 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <time.h>
+#define N 50 //definiuje liczbę losowo generowanych liczb w tablicy
 
-struct die { //Kostka
-    char type;
-    int minRoll;
-    int maxRoll;
-    int effectIndex;
-};
+void swap(int array[], int indexA, int indexB) { //zamienia liczby o podanych indeksach, funkcja dostosowana tak, by pasowala do wszystkich funkcji sortowania
+    int buffer;
 
-struct enemy {
-    char* name;
-    int diceCount;
-    struct die dice[];
-};
+    buffer = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = buffer;
+}
 
-enum readMode { //Tryb wczytywania danych z pliku
-    enemy,
-    dice
-};
+void merge(int array[], int begin, int mid, int end) { //łączy dwie rozdzielone tablice w jedną tymczasową, następnie główną opracowywaną tablicę nadpisuje danymi z tymczasowej
+    int origArrayBegin, origArrayMid, tempArrayBegin;
+    int tempArray[N]; //tablica tymczasowa
 
-void stringLowercase(char* string) {
-    for (int i = 0; string[i]; i++) {
-        string[i] = tolower(string[i]);
+    origArrayBegin = begin; origArrayMid = mid;
+
+    for (tempArrayBegin = begin; tempArrayBegin <= end; tempArrayBegin++) {
+        if (((origArrayMid <= end) && (array[origArrayMid] < array[origArrayBegin])) || (origArrayBegin == mid)) { //sprawdza z której połowicznej tablicy wpisać liczbę do tablicy
+            tempArray[tempArrayBegin] = array[origArrayMid++];                                                     //tymczasowej, zależnie od tego która liczba jest większa
+        } else {                                                                                                   //i uzupełnia połowę tablicy tymczasowej zawierającą
+            tempArray[tempArrayBegin] = array[origArrayBegin++];                                                   //liczby wyższe od środkowej lub niższe
+        }
+    }
+
+    for (tempArrayBegin = begin; tempArrayBegin <= end; tempArrayBegin++) { //nadpisuje tablicę oryginalną tą tymczasową, która jest ułożona wzrastająco
+        array[tempArrayBegin] = tempArray[tempArrayBegin];
     }
 }
 
-void queryFileLocation(enum readMode mode, char* fileLocation) { //Pyta o lokację brakującego pliku
-                                                                 //lub zamyka program
-    char* input;
+void mergeSort(int array[], int begin, int end) { //sortowanie przez scalanie, działające rekurencyjnie
+    int mid = (begin+end+1)/2;
 
-    puts("Enter the missing ");
-    switch(mode) {
-        case enemy:
-            printf("enemy "); break;
-        case dice:
-            printf("dice "); break;
+    if (begin == end) { //zatrzymuje funkcję, gdy nie da się już dalej podzielić tablicy, tzn utworzony podział zawierałby tylko jedną liczbę
+        return;
     }
 
-    puts("data location or (E)xit program..."); //W nawiasach oznaczone sa
-                                                //komendy ktore mozna wpisac
-    scanf("%s", input);                                       //i ich skroty
-    stringLowercase(input); //Ignoruje wielkosc liter
-
-    if (strcmp(input, "e") || strcmp(input, "exit")) { //Sprawdza czy wpisano e
-        puts("Quitting program...");                   //lub exit
-        exit(0);
-    }
-
-    fileLocation = input;
-
+    mergeSort(array, begin, mid-1); //dzieli poprzez rekurencyjne wywołanie funkcji tablicę na połowę
+    mergeSort(array, mid, end);
+    merge(array, begin, mid, end);
 }
 
-void dataScan(enum readMode mode) { //Pobieranie danych z plików tekstowych
+void queryNumber(int* searchNum) { //wypytuje użytkownika o to, jaką liczbę wyszukać i sprawdza czy jest w zakresie obsługiwanym przez program i powtarza jeśli się nie zgadza
+    while (*searchNum <= 0 || *searchNum > 50) {
+        puts("which number to search?:");
+        scanf("%d", searchNum);
+    }
+}
 
-    FILE *gameFile = NULL;
-    char* fileLocation;
+void printArray(int array[]) { //wypisuje wszystkie w kolejności liczby w tablicy która jest opracowywana
+    int i;
 
-    switch(mode) {
-        case enemy:
-            fileLocation = "enemies.txt";
-            puts("Loading enemy data...");
+    for (i = 0; i < N; i++) {
+        printf("%d ", array[i]);
+    }
 
-            while (gameFile == NULL) {
-                gameFile = fopen(fileLocation, "r");
-                puts("Enemy data file not found...!");
-                queryFileLocation(mode, fileLocation);
+    printf("\n");
+}
+
+void bubbleSort(int array[]) { //sortowanie bąbelkowe
+    int i, j;
+
+    for (i = N - 1; i > 0; i--) { //zakres końcowy się pomniejsza z każdą pętlą, ponieważ po każdej iteracji największa liczba w zakresie zostaje na ostatnim miejscu
+        for (j = 0; j < i; j++) {
+            if (array[j] > array[j+1]) {
+                swap(array, j, j+1);
             }
-
-
-
+        }
     }
 }
 
-int main () {
+void selectSort(int array[]) { //sortowanie przez wybieranie
+    int i, j;
 
-    puts("Starting game...");;
-    dataScan(enemy); dataScan(dice);
-
-
-
+    for (i = 0; i < N; i++) {
+        for (j = i+1; j < N; j++){ //każda liczba z indeksu j jest porównywana z jedną liczbą z indeksu i, która jest przyjęta jako najmniejsza
+            if (array[i] > array[j]) { //jeśli i okazuje się jednak większe, następuje zamiana
+                swap(array, i, j);
+            }
+        }
+    }
 }
 
-/*
- * stats:
- * speed - increase minimum damage on a die, raises accuracy by 6%
- * attack - increase maximum damage on a die, 12 attack adds an additional die
- * defence - increase maximum defence on a defence die, 12 defence adds an additional defence die
- * wellbeing - increase minimum defence on a die, raises the amount of health by 2
- *
- * piercing attack - when all dice show the same number
- * dodge - when all defence dice show the same number
- *
- * items:
- * glasses - adds 20% accuracy, lost upon receiving a certain amount of damage, always pierces when rolling at least 2 2s
- * double edged sword - increases damage, deals one damage to self
- * equalizer - raises minimal roll
- * d6 - adds a 1 to 6 die
- * d3 - adds a 1 to 3 die
- * splitter - splits 1 die into 2 dice, each only does half of max damage of the original die
- * booze - increases damage, lowers perception
+int linearSearch(int array[], int searchNum) { //wyszukiwanie liniowe, zwraca wartość -1 jako sygnał tego, że nie znaleziono liczby
+    int i;
+
+    for (i = 0; i < N; i++) { //poprostu przechodzi przez całą tabelę i zwraca indeks pierwszej z kolei liczby takiej samej co wyszukiwana
+        if (array[i] == searchNum) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int binarySearch(int array[], int searchNum, int begin, int end) { //wyszukiwanie binarne, zwraca wartość -1 jako sygnał tego, że nie znaleziono liczby
+    int mid = (begin+end)/2;
+
+    if (array[mid] == searchNum) {
+        return mid;
+    } else if (array[mid] > searchNum) {
+        end = mid-1;
+    } else if (array[mid] < searchNum) {
+        begin = mid+1;
+    }
+    printf("begin = %d\n", begin); printf("end = %d\n", end);
+
+    if (begin > end) {
+        return -1;
+    } else {
+        return binarySearch(array, searchNum, begin, end); //wyszukiwanie działa rekurencyjnie, wyszukiwany zakres jest dzielony na coraz mniejsze połowy dopóki
+    }                                                      //nie zostanie w zakresie tylko jedna liczba, która będzię tą wyszukiwaną
+}                                                          //zakres połówek przesuwany jest w zależności od tego czy liczba środkowa jest mniejsza lub większa od szukanej
+
+int main() {
+    srand(time(NULL));
+    int array[N];
+    int i, input, searchNum, searchIndex;
+    searchNum = 0;
+
+    for (i = 0; i < N; i++) { //generacja losowej tablicy zawierającej liczby od 1 do 50
+        array[i] = (rand() % 50) + 1;
+    }
+    printArray(array); //pokazuje tablicę przed operacją, dalej w kodzie jest też wywołany wydruk tablicy po operacji
+
+    puts("choose option (enter the corresponding number):"); //instrukcja opcji w programie, należy wpisać numer danej operacji
+    puts("1) bubble sort");
+    puts("2) selection sort");
+    puts("3) linear search");
+    puts("4) binary search");
+    puts("5) merge sort");
+    scanf("%d", &input);
+
+    switch (input) {
+        case 1:
+            bubbleSort(array);
+            printArray(array);
+            break;
+        case 2:
+            selectSort(array);
+            printArray(array);
+            break;
+        case 3:
+            queryNumber(&searchNum);
+            searchIndex = linearSearch(array, searchNum);
+            if(searchIndex == -1) { //zwrócony indeks -1 sygnalizuje brak liczby w tablicy
+                puts("number not found in array");
+            } else {
+                printf("number %d found at index %d\n", searchNum, searchIndex); //pokazywany jest indeks liczby w kodzie, dlatego np gdy zostanie wskazana ostatnia liczba w
+            }                                                                    //tablicy, zostanie podany indeks 49
+            break;
+        case 4:
+            queryNumber(&searchNum);
+            bubbleSort(array); //tablica musi być przesortowana przed użyciem wyszukiwania binarnego
+            printArray(array);
+            searchIndex = binarySearch(array, searchNum, 0, N-1); //N-1, żeby nie próbowało operować na indeksie np. 50
+            if(searchIndex == -1) { //zwrócony indeks -1 sygnalizuje brak liczby w tablicy
+                puts("number not found in array");
+            } else {
+                printf("number %d found at index %d\n", searchNum, searchIndex); //pokazywany jest indeks liczby w kodzie, dlatego np gdy zostanie wskazana ostatnia liczba w
+            }                                                                    //tablicy, zostanie podany indeks 49
+            break;
+        case 5:
+            mergeSort(array, 0, N-1); //N-1, żeby nie próbowało operować na indeksie np. 50
+            printArray(array);
+            break;
+        default:
+            puts("wrong input");
+            break;
+    }
+
+    return 0;
+}
